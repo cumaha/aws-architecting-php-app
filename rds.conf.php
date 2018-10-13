ce388285b52a1d139ef69acac6bacfa3f3721624
Darrel <darrel@Darrells-MacBook-Air.local>
2018-10-13 14:43:32 -0400

    initial commit

---
+++ rds.conf.php
@@ -0,0 +1,6 @@
+<?php
+$RDS_URL="RDS_ENDPOINT_URL";
+$RDS_DB="mydb";
+$RDS_user="admin";
+$RDS_pwd="admin1234";
+?>
---
+++ sql/addressbook.sql
@@ -0,0 +1,2 @@
+CREATE TABLE address (id INT(4) NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(45), phone VARCHAR(30), email VARCHAR(30));
+INSERT INTO address (name, phone, email) VALUES ( "Johnson, Roberto", "123-456-7890", "robertoj@someaddress.com"), ( "Doe, Jane", "010-110-1101", "janed@someotheraddress.org" );
